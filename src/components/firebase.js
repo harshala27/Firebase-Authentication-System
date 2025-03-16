78d03e035b8bce1e6d1852e98bd63a770b3df4d7
harshala gharat <hgharat735@gmail.com>
2025-03-16 16:29:00 +0530

    Updated firebase config to use environment variables

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDF--vL_srJYmuB3hTwx6BaMFIEOPbr7_c",
-  authDomain: "login-auth-a4fca.firebaseapp.com",
-  projectId: "login-auth-a4fca",
-  storageBucket: "login-auth-a4fca.firebasestorage.app",
-  messagingSenderId: "1037020094534",
-  appId: "1:1037020094534:web:b4bbdbde89b17de2e2aa47",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
